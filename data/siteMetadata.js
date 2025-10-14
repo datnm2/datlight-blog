6ed80565baca8a10d801910edf81e288984691ef
Dat09VN <nmdat.it@gmail.com>
2025-10-14 17:49:39 +0700

    feat: personalize

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: "Dat Light's Blog",
+  author: 'Dat Nguyen',
+  headerTitle: "Dat Light's Blog",
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
